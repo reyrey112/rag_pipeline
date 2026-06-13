48a33b35eb419f46c6bfa0ca7440e1296c385480
Reyden Davies <reydencdavies@gmail.com>
2026-06-13 17:10:46 -0400

    airflow starting script

---
+++ airflow.sh
@@ -0,0 +1,22 @@
+export AIRFLOW_HOME=~/rag_pipeline/airflow
+echo 'export AIRFLOW_HOME=~/rag_pipeline/airflow' >> ~/.bashrc
+source ~/.bashrc
+airflow db migrate
+
+if [ -f ~/rag_pipeline/.env ]; then
+    # Automatically export all variables defined from this point forward
+    set -o allexport
+    source ~/rag_pipeline/.env
+    # Turn off automatic exporting
+    set +o allexport
+else
+    echo ".env file not found"
+    exit 1
+fi
+
+airflow connections add 'databricks_default' \
+    --conn-type 'databricks' \
+    --conn-host "$DATABRICKS_HOST" \
+    --conn-password "$DATABRICKS_TOKEN"
+
+airflow standalone
